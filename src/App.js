9ccab55cf2826cebf2689301272ae0e4c0708d99
Raghul297 <raghul.a1017@gmail.com>
2025-04-04 23:32:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UploadForm from './components/UploadForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-container">
+      <UploadForm />
+    </div>
+  );
+}
+
+export default App;
