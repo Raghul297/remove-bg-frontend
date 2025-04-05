1c59c79e61498d2cd8a79ec6b10c1c588d7b69be
Raghul297 <raghul.a1017@gmail.com>
2025-04-05 15:30:20 +0530

    Delete src directory

--- src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE,
-  timeout: 60000,
-});
-
-export const uploadImage = (formData) => API.post('/upload', formData);
